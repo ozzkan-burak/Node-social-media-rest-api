5900a7607bc7e37c63461914626a1fadc5a83f07
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-15 00:12:01 +0300

    user register ready

--- router/auth.js
+++ router/auth.js
@@ -1,20 +1,33 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt"); 
 const User = require("../models/User");
 
+ // REGISTER
 router.post("/register", async (req, res)=> {
-  const newUser = await new User({
-    username : req.body.username,
-    email : req.body.email,
-    password : req.body.password
-  });
 
   try {
+    // generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // create new user
+    const newUser = await new User({
+      username : req.body.username,
+      email : req.body.email,
+      password : hashedPassword
+    });
+
+    // save user and return response
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err){
     console.log(err)
   }
-  
+
 });
+
+// LOGIN
+
+
 
 module.exports = router;
