1f2da0f3fbbec50ad5a40ee2a47fa88ec27d57d2
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-22 00:50:22 +0300

    create follower user

--- router/users.js
+++ router/users.js
@@ -50,6 +50,26 @@ router.get("/:id", async (req, res)=> {
   }
 })
 // get all users
+router.put("/:id/follow", async (req, res)=> {
+  if(req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if(!user.followers.includes(req.body.userId)) {
+        await user.updateOne({$push: {followers: req.body.userId}});
+        await currentUser.updateOne({$push: {followings: req.body.userId}});
+        res.status(200).json("User has been followed");
+      } else {
+        res.stattus(403).json("you allready follow this.user");
+      }
+    } catch(err) {
+      res.status(500).json(err)
+    }
+  } else {
+      res.status(403).json("You cant follow yourself");
+  }
+})
 // follw a user
 // unfollw a user
 
